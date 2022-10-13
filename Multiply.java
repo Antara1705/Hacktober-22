0fcdb645a971b30a6e243d74d36484f78528d947
Rishiraj Dey <48823057+ramraj13@users.noreply.github.com>
2022-10-13 23:29:05 +0530

    Add files via upload

---
+++ Multiply.java
@@ -0,0 +1,12 @@
+public class Multiply {
+
+    public static void main(String[] args) {
+
+        float f = 2.5f;
+        float s = 6.0f;
+
+        float p = f * s;
+
+        System.out.println("The product is: " + p);
+    }
+}
